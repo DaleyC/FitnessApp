ceddd538e0ccec7433eac47c601dd9baa545c1c6
Daley <ctlncardwell@gmail.com>
2016-07-26 09:50:09 -0400

    Initial commit

---
+++ FitnessApp.Web/app/app.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', ['ui.router', 'ui.bootstrap']);
+
+    angular.module('app').config(config);
+    function config($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state('base', {
+                url: "/",
+                templateUrl: "app/base/baseController.html"
+            })
+    }
+})();
---
+++ FitnessApp.Web/app/base/BaseController.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').controller('BaseController', BaseController);
+
+    function BaseController() {
+        var vm = this;
+    }
+})();
