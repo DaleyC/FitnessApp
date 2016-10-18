389bba352b9f8aca048a357530cf397a08f796bc
--global <CtlnCardwell@gmail.com>
2016-10-18 14:42:54 -0400

    fix(nutritionTracker): add custom directive to fix ui-select2 validation

---
+++ FitnessApp/wwwroot/app/nutritionTracker/fixSelect2Validation.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').directive('fixSelect2Validation', fixSelect2Validation);
+
+    function fixSelect2Validation() {
+        var directive = {
+            link: link,
+            restrict: 'A',
+            scope: {
+                ngModel: '='
+            }
+        };
+
+        return directive;
+
+        function link(scope, element, attrs, ctrl) {
+            scope.$watch(function () {
+                return scope.ngModel;
+            },
+            function (newValue, oldValue) {
+                if (scope.ngModel) {
+                    return element.siblings().addClass("removeBorder");
+                }
+                element.siblings().removeClass("removeBorder");
+            });
+        }
+    }
+})();
