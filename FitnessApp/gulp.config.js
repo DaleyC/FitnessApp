17aa7937112200a9a1432fb085f9f5b1ca8f9971
--global <CtlnCardwell@gmail.com>
2016-11-08 12:24:30 -0700

    refactor(app): now use status library instead of cdn for third party libraries

--- FitnessApp/gulp.config.js
+++ FitnessApp/gulp.config.js
@@ -1,18 +1,51 @@
 ﻿module.exports = function () {
-    var root = './wwwroot/';
-    var rootApp = root + 'app/';
-    var server = './wwwroot/app/';
-    var temp = './.tmp/';
+    var client = './client/';
+    var clientApp = client + 'app/';
+    var report = "./report/";
     var config = {
 
         /** 
         * File paths
         * */
         alljs: [
-            rootApp + '**/*.js',
-            './*.js',
-            '!' + '**/scripts/**'
-        ]
+            clientApp + '**/*.js'
+        ],
+        report: report
+        /*
+         * Karma and testing settings
+         */
+
     };
+
+    config.karma = getKarmaOptions();
+
     return config;
+
+    /////////////////
+
+    function getKarmaOptions() {
+        var options = {
+            files: [].concat(
+                './client/scripts/jquery-3.1.1.min.js',
+                './client/scripts/angular-1.5.8.min.js',
+                //'./client/scripts/sinon.js',
+                './client/scripts/bard.min.js',
+                clientApp + 'app.js',
+                clientApp + '**/*.js',
+                clientApp + 'exerciseTracker/**/*.js'
+                ),
+            exclude: [],
+            coverage: {
+                dir: report + 'coverage',
+                reporters: [
+                    { type: 'html', subdir: 'report-html' },
+                    { type: 'lcov', subdir: 'report-lcov' },
+                    { type: 'text-summary' }
+                ]
+            },
+            preprocessors: {}
+        };
+        options.preprocessors[clientApp + '**/!(*.spec)+(.js)'] = ['coverage'];
+        return options;
+    }
 };
